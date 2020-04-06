514596f09418d43ae798aa59795c11d1e5f9b064
Mayra-RR <maiira.regalado@gmail.com>
2020-04-06 17:52:25 -0500

    Updates

---
+++ precache-manifest.ff0876aefa1d5af8682a783d936998bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d37181b8cb19fd993d1c014d679552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb84d88fbc2dc09439b3",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ce70fe591364ed00b48a",
+    "url": "/static/js/2.537fb2ee.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/static/js/2.537fb2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb84d88fbc2dc09439b3",
+    "url": "/static/js/main.3da74d28.chunk.js"
+  },
+  {
+    "revision": "25de4f753e0c8b3aa3aa",
+    "url": "/static/js/runtime-main.6d996a2c.js"
+  }
+]);
